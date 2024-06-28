161d63152ef30b306fa14e0c97cc1c94072153af
savom <56549021+savom@users.noreply.github.com>
2024-06-28 15:52:41 +0900

    Add files via upload

---
+++ CPP folder/WinAPIClass 4/WinAPIClass/WinAPIClass.cpp
@@ -0,0 +1,42 @@
+﻿#include "WinApp.h"
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+    WinApp app;
+    app.RegisterWindowClass(hInstance);
+    if (!app.CreateMainWindow(nCmdShow))
+    {
+        return FALSE;
+    }
+
+    return app.MessageLoop();
+
+}
+
+
+
+// 정보 대화 상자의 메시지 처리기입니다.
+//INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+//{
+//    UNREFERENCED_PARAMETER(lParam);
+//    switch (message)
+//    {
+//    case WM_INITDIALOG:
+//        return (INT_PTR)TRUE;
+//
+//    case WM_COMMAND:
+//        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+//        {
+//            EndDialog(hDlg, LOWORD(wParam));
+//            return (INT_PTR)TRUE;
+//        }
+//        break;
+//    }
+//    return (INT_PTR)FALSE;
+//}
